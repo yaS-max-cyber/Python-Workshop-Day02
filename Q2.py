ee9408f7af1025f84a39e1a56639c27e61bdfd84
Yasith Induwara <yasithidu@gmail.com>
2025-05-28 18:51:38 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,12 @@
+def lager(a,b):
+    if(a>b):
+        print("max number is:",a)
+        return a
+    else:
+        print("max number is:",b)
+        return b
+ 
+a = int(input("enter num 1:"))
+b = int(input("enter num 2:"))
+
+lager(a,b)  
